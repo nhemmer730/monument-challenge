a7af6f8113e495e9127d561c1b851e572a428194
nhemmer730 <nhemmer730@gmail.com>
2024-02-18 14:19:26 -0500

    add time/date JS

--- src/index.js
+++ src/index.js
@@ -1,87 +1,37 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
-}
+let now = new Date();
+let currentDateTime = document.querySelector(".date");
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDate();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-  return days[day];
-}
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
 
-function getForecast(coordinates) {
-  let apiKey = "9faada74e0td93b882032b77odf27ad4";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?long=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}`;
-  axios.get(apiUrl).then(displayForecast);
-}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
 
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#weather-icon");
-  let celsiusTemperature = response.data.daily.temperature;
-
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.city;
-
-  descriptionElement.innerHTML = response.data.condition.description;
-  dateElement.innerHTML = formatDate(response.data.time / 1000);
-  iconElement.setAttribute("src", `${response.data.condition.icon_url}`);
-  iconElement.setAttribute("alt", response.data.condition.description);
-
-  getForecast(response.data.coordinates);
-}
-
-function search(city) {
-  let apiKey = "9faada74e0td93b882032b77odf27ad4";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(displayTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-function updateTime() {
-  let romeElement = document.querySelector("#rome");
-  if (romeElement) {
-    let romeDateElement = romeElement.querySelector(".date");
-    let romeTimeElement = romeElement.querySelector(".time");
-    let romeTime = moment().tz("Europe/Rome");
-
-    romeDateElement.innerHTML = moment().format("MMMM Do YYYY");
-    romeTimeElement.innerHTML = moment().format("h:mm [<small>A</small>]");
-  }
-}
-
-updateTime();
-setInterval(updateTime, 1000);
-
-let citySelectElement = document.querySelector("#city");
-citySelectElement.addEventListener("change", search);
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+hours = hours < 10 ? "0" + hours : hours;
+minutes = minutes < 10 ? "0" + minutes : minutes;
+date.innerHTML = `${day} ${month} ${date}, ${hours};${minutes}, ${year}`;
