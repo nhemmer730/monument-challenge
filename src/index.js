488dcb70f9c4b0affbdd0ef6e161984bc4aa4416
nhemmer730 <nhemmer730@gmail.com>
2024-02-18 14:26:36 -0500

    Update time JS and css

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 let now = new Date();
-let currentDateTime = document.querySelector(".date");
-
+let h5 = document.querySelector("h5");
 let date = now.getDate();
 let hours = now.getHours();
 let minutes = now.getMinutes();
 let year = now.getFullYear();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
 
 let months = [
   "Jan",
@@ -26,12 +14,15 @@ let months = [
   "Jun",
   "Jul",
   "Aug",
-  "Sept",
+  "Sep",
   "Oct",
   "Nov",
   "Dec",
 ];
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+let day = days[now.getDay()];
 let month = months[now.getMonth()];
-hours = hours < 10 ? "0" + hours : hours;
-minutes = minutes < 10 ? "0" + minutes : minutes;
-date.innerHTML = `${day} ${month} ${date}, ${hours};${minutes}, ${year}`;
+
+h5.innerHTML = `${day} ${month} ${date}, ${year} ${hours}:${minutes}`;
